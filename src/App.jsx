eeeb1e2a2724e29a92538b3c410bc30dff269687
omerbenarieh <omerrbenarieh@gmail.com>
2023-09-07 21:46:41 +0300

    Protecting App Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 // Context API
 import { CitiesProvider } from './contexts/CitiesContext';
 import { AuthProvider } from './contexts/FakeAuthContext';
-// App Components
+
+// App Pages
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+
+// App Components
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
+import ProtectedRoutes from './pages/ProtectedRoutes';
 
 function App() {
   return (
@@ -26,7 +30,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoutes>
+                  <AppLayout />
+                </ProtectedRoutes>
+              }
+            >
               <Route index element={<Navigate replace to={'cities'} />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
